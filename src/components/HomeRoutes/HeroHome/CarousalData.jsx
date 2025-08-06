257c1ec878d5dd5379abe051ef7a9556e13caea3
webtesting22 <148150265+webtesting22@users.noreply.github.com>
2025-08-06 13:07:17 +0530

    Updated Carousal Image on heroHomeSlides

--- src/components/HomeRoutes/HeroHome/CarousalData.jsx
+++ src/components/HomeRoutes/HeroHome/CarousalData.jsx
@@ -1,19 +1,19 @@
 const CarousalData = [
     {
         id: 1,
-        image: "https://s3.ap-south-1.amazonaws.com/prepseed/prod/ldoc/media/GayaCarousalImage1.png",
+        image: "https://images.unsplash.com/photo-1743210262905-b60e9cfd60ed?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         title: "FLOORING &",
         description: "ACOUSTIC SOLUTION"
     },
     {
         id: 2,
-        image: "https://s3.ap-south-1.amazonaws.com/prepseed/prod/ldoc/media/GayaCarousalImage1.png",
+        image: "https://images.unsplash.com/photo-1600166931532-604e927c794b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         title: "PREMIUM",
         description: "FLOORING MATERIALS"
     },
     {
         id: 3,
-        image: "https://images.unsplash.com/photo-1586023492125-27b2c045efd7?ixlib=rb-4.0.3&auto=format&fit=crop&w=1000&q=80",
+        image: "https://images.unsplash.com/photo-1684145690468-a2cecc2f078f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         title: "PROFESSIONAL",
         description: "INSTALLATION SERVICE"
     }

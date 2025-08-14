199619372fbac55074dc8a9f60b2cbfe3cf6561a
webtesting22 <148150265+webtesting22@users.noreply.github.com>
2025-08-14 18:13:34 +0530

    new data added on pages and mobile responsive

--- src/components/OtherComponents/Categories/Layouts/BroadloomCarpetsLayout.vue
+++ src/components/OtherComponents/Categories/Layouts/BroadloomCarpetsLayout.vue
@@ -26,8 +26,8 @@
                             disableOnInteraction: false,
                             pauseOnMouseEnter: true,
                         }" :breakpoints="{
-                            320: { slidesPerView: 1, spaceBetween: 15 },
-                            768: { slidesPerView: 2, spaceBetween: 20 },
+                            320: { slidesPerView: 4, spaceBetween: 10 },
+                            768: { slidesPerView: 4, spaceBetween: 10 },
                             1024: { slidesPerView: 2, spaceBetween: 30 },
                             1200: { slidesPerView: 3, spaceBetween: 30 }
                         }" :navigation="{

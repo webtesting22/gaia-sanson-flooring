5bdd947e5bc40c587dba7b652bdd2ee492621070
webtesting22 <148150265+webtesting22@users.noreply.github.com>
2025-08-22 19:24:03 +0530

    Updated changes

--- src/components/HomeRoutes/HeroHome/CarousalData.jsx
+++ src/components/HomeRoutes/HeroHome/CarousalData.jsx
@@ -7,13 +7,13 @@ const CarousalData = [
     },
     {
         id: 2,
-        image: "https://images.unsplash.com/photo-1600166931532-604e927c794b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        image: "https://s3.ap-south-1.amazonaws.com/prepseed/prod/ldoc/media/HeroHome1.png",
         title: "PREMIUM",
         description: "FLOORING MATERIALS"
     },
     {
         id: 3,
-        image: "https://images.unsplash.com/photo-1684145690468-a2cecc2f078f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        image: "https://s3.ap-south-1.amazonaws.com/prepseed/prod/ldoc/media/HeroHome1.png",
         title: "PROFESSIONAL",
         description: "INSTALLATION SERVICE"
     }

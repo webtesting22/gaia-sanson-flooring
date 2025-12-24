19913adf567e04d29ffda3843907c520f020c721
Abhi solanki <abhi85076@gmail.com>
2025-12-24 18:12:09 +0530

    changed image in home-page-comparison

--- src/components/HomeRoutes/HomeImageComparison/HomeImageComparison.vue
+++ src/components/HomeRoutes/HomeImageComparison/HomeImageComparison.vue
@@ -8,8 +8,8 @@ export default {
         title:
           "Showcasing our products in various settings for easy comparison",
         leftImage: {
-          src: "https://s3.ap-south-1.amazonaws.com/prepseed/prod/ldoc/media/AccousticHome.jpg",
-          label: "ACOUSTIC PET PANEL",
+          src: "/Images/CARPET_TILES.png",
+          label: "CARPET TILES",
         },
         rightImage: {
           src: "https://s3.ap-south-1.amazonaws.com/prepseed/prod/ldoc/media/SHOWCASINGOURCARPETS2.png",
